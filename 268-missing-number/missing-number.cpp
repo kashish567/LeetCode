ee9d4401efdaa414940c96d5887dd368e08530e9
Kashish Bhanushali <121213612+kashish567@users.noreply.github.com>
2024-07-14 10:57:01 +0530

    Time: 16 ms (38.92%) | Memory: 20.8 MB (26.42%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
+    #define ll long long
+
     int missingNumber(vector<int>& nums) {
+        
         int n = nums.size();
-        vector<int>v(n+1,-1);
-        for(int i =0;i<nums.size();i++){
-            v[nums[i]] = nums[i];
-        }
-        for(int i =0;i<v.size();i++){
-            if(v[i]==-1)return i;
-        }
-        return 0;
+        int xr1 = 0;
+        int xr2 = 0;
+
+        for(int i=1; i<=n; i++) {
+            xr1 = xr1^nums[i-1];
+            xr2 = xr2^i;
+        } 
+
+        return xr1^xr2; 
     }
 };
-
