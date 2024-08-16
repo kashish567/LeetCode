531a69eb3a95a865f9bf161b356f6e34f5a258cc
Kashish Bhanushali <121213612+kashish567@users.noreply.github.com>
2024-08-16 20:12:30 +0530

    Time: 4 ms (63.12%) | Memory: 19.7 MB (34.52%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,51 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-       
-	    // if list1 happen to be NULL
-		// we will simply return list2.
-        if(list1 == NULL)
-            return list2;
-		
-		// if list2 happen to be NULL
-		// we will simply return list1.
-        if(list2 == NULL)
-            return list1;
+        // Create a dummy node to serve as the start of the merged list
+        ListNode dummy(0);
+        ListNode* current = &dummy;  // Pointer to build the merged list
         
-        ListNode * ptr = list1;
-        if(list1 -> val > list2 -> val)
-        {
-            ptr = list2;
-            list2 = list2 -> next;
+        // Iterate through both lists until one is exhausted
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val <= list2->val) {
+                current->next = list1;  // Append the smaller node to the merged list
+                list1 = list1->next;    // Move list1 pointer to the next node
+            } else {
+                current->next = list2;  // Append the smaller node to the merged list
+                list2 = list2->next;    // Move list2 pointer to the next node
+            }
+            current = current->next;  // Move the current pointer forward
         }
-        else
-        {
-            list1 = list1 -> next;
-        }
-        ListNode *curr = ptr;
         
-		// till one of the list doesn't reaches NULL
-        while(list1 &&  list2)
-        {
-            if(list1 -> val < list2 -> val){
-                curr->next = list1;
-                list1 = list1 -> next;
-            }
-            else{
-                curr->next = list2;
-                list2 = list2 -> next;
-            }
-            curr = curr -> next;
-                
+        // If one of the lists is not exhausted, append the remaining nodes
+        if (list1 != nullptr) {
+            current->next = list1;
+        } else {
+            current->next = list2;
         }
-		
-		// adding remaining elements of bigger list.
-        if(!list1)
-            curr -> next = list2;
-        else
-            curr -> next = list1;
-            
-        return ptr;
-       
+        
+        // Return the merged list, starting from the node after the dummy node
+        return dummy.next;
     }
 };
